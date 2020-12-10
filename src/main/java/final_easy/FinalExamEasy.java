0370b2285a2ffb40ce333e2f73f4d728e5a8f23f
Lunde CHEN <lunde.chen@yahoo.fr>
2020-12-10 16:36:59 +0800

    m

--- src/main/java/final_easy/FinalExamEasy.java
+++ src/main/java/final_easy/FinalExamEasy.java
@@ -1,16 +1,20 @@
 package final_easy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 
-class MyData{
+class MyData {
     // ATTENTION: Please don't modify anything on the MyData class.
-    // Or else, you will fail the easy part directly.
     int value;
 
     public MyData(int v){
         this.value = v;
+    }
+
+    public String toString(){
+        return "(" + this.value + ")";
     }
 }
 
@@ -28,28 +32,27 @@ public class FinalExamEasy {
         1, 3, 5, 7, 9 are odd.
         Please don't modify the return type (boolean).
         */
-        return false;
+        return false; // TODO return something else
     }
 
-    public void printDayOfTheWeekInChinese(int dayNumber){
+    public String getDayOfTheWeek(int dayNumber){
         /* TODO Question 2
-         Write a program called printDayOfTheWeekInChinese which prints
-         “星期一”, “星期二”, ... “星期日” if the int variable
+         Write a program called getDayOfTheWeek which returns
+         “Monday”, “Tuesday”, ... “Sunday” if the int variable
          "dayNumber" is 1, ..., 7, respectively.
-         Otherwise, it shall print "计算失败".
+         Otherwise, it shall return "FinalExamDay".
          Please use a "switch-case-default" statement.
-         Please don't modify the return type (void).
+         Please don't modify the return type (String).
          Please use println instead of print.
          */
+        return "FinalExamDay"; // TODO return something else
     }
 
     public double computePIWithSeriesExpansion(){
         /* TODO Question 3
         We know that PI = 4 * (1/1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + ...)
         Use a for or while loop to calculate this value.
-        The loop stops when either of the two conditions is fulfilled:
-        1. we are at ... -1/19 + 1/21.
-        2. or, if abs(pi - 3.141592663) <= 0.000000001
+        The loop stops when we are at ... -1/19 + 1/21.
         Please use a for or while loop statement.
         Please don't modify the return type (double).
         */
@@ -57,7 +60,26 @@ public class FinalExamEasy {
         return pi;
     }
 
-    public void Question4(){
+    public void print2DIntArray(int[][] arr2d){
+        // This function helps you to debug if you want to
+        // print a two-dimensional array of int type.
+        int m = arr2d.length;
+        if(m == 0) return;
+        int n = arr2d[0].length;
+        if(n == 0) return;
+        for(int i = 0; i < m; i ++){
+            for(int j = 0; j < n; j ++){
+                if(arr2d[i][j] == 0)
+                    System.out.print("   ");
+                else
+                    System.out.printf("%3d", arr2d[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public int[][] Question4(int size){
+        int[][] arr2d = new int[size][size * 2 - 1];
         /* TODO Question 4
         Write
         1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
@@ -71,28 +93,17 @@ public class FinalExamEasy {
         Please don't modify the return type (void).
         For each line, please use println instead of print.
         */
+        print2DIntArray(arr2d);
+        return arr2d;
     }
 
-    public void Question51(){
+    public int[][] Question5(int size){
+        int[][] arr2d = new int[size][size];
         /*
         Write a programs called NumberPatternX
          that prompts user for the size
          (a non-negative integer in int); and prints the pattern as shown:
-         H g F e D c B a
-         g F e D c B a
-         F e D c B a
-         e D c B a
-         D c B a
-         c B a
-         B a
-         a
-         G f E d C b A
-         F e D c B a
-         E d C b A
-         D c B a
-         C b A
-         B a
-         A
+         size = 8, then the arr2d will be printed as:
          8 7 6 5 4 3 2 1
          7 6 5 4 3 2 1
          6 5 4 3 2 1
@@ -102,94 +113,60 @@ public class FinalExamEasy {
          2 1
          1
          */
+        print2DIntArray(arr2d);
+        return arr2d;
     }
 
-    public void Question52(){
+    public int[][] Question6(int size){
+        int[][] arr2d = new int[size][size * 2];
         /*
-        Write a programs called NumberPatternX
-         that prompts user for the size
-         (a non-negative integer in int); and prints the pattern as shown:
-         H g F e D c B a
-         g F e D c B a
-         F e D c B a
-         e D c B a
-         D c B a
-         c B a
-         B a
-         a
-         G f E d C b A
-         F e D c B a
-         E d C b A
-         D c B a
-         C b A
-         B a
-         A
-         8 7 6 5 4 3 2 1
-         7 6 5 4 3 2 1
-         6 5 4 3 2 1
-         5 4 3 2 1
-         4 3 2 1
-         3 2 1
-         2 1
-         1
+         8 7 6 5 4 3 2 1 8 7 6 5 4 3 2 1
+         7 6 5 4 3 2 1   7 6 5 4 3 2 1
+         6 5 4 3 2 1     6 5 4 3 2 1
+         5 4 3 2 1       5 4 3 2 1
+         4 3 2 1         4 3 2 1
+         3 2 1           3 2 1
+         2 1             2 1
+         1               1
          */
+        print2DIntArray(arr2d);
+        return arr2d;
     }
 
-    public void Question53(){
+    public HashMap<String, MyData> Question7(){
+        HashMap<String, MyData> hashMap = new HashMap<String, MyData>();
         /*
-        Write a programs called NumberPatternX
-         that prompts user for the size
-         (a non-negative integer in int); and prints the pattern as shown:
-         H g F e D c B a
-         g F e D c B a
-         F e D c B a
-         e D c B a
-         D c B a
-         c B a
-         B a
-         a
-         G f E d C b A
-         F e D c B a
-         E d C b A
-         D c B a
-         C b A
-         B a
-         A
-         8 7 6 5 4 3 2 1
-         7 6 5 4 3 2 1
-         6 5 4 3 2 1
-         5 4 3 2 1
-         4 3 2 1
-         3 2 1
-         2 1
-         1
+        Hashmap<String, MyData>
+        One: 1
+        Two: 9
+        Three: 27
+        Four: 81
+        Five: 243
          */
+        return hashMap;
     }
 
-
-
-    public void Question6(){
+    public MyData[] Question8(MyData[] arrMyData){
         /*
          Sort an array of MyData, using bubblesort or selectionsort.
-         Please don't modify anything on the MyData class.
-         You can add functions to the FinalExamEasy class.
+         ATTENTION: Please don't modify anything on the MyData class.
+         On the contrary, you can add functions to the FinalExamEasy class.
         */
+        return arrMyData;
     }
 
-    public void Question7(){
+    public MyData Question9(MyData[] arrMyData){
         /*
          Find the 2nd maximum number of an array of MyData
         */
+        return null; // TODO return something else
     }
 
-    public void Question8(){
+    public MyData Question10(MyData[] arrMyData){
         /*
-        Find the MyData object of an array of MyData whose absolute is the most in proximity to 100.
+        Find the MyData object of an array of MyData whose value is the most in proximity to 10.
         */
+        return null; // TODO return something else
     }
 
-    public static void main(String[] args) {
-
-
-    }
 }
---
+++ src/main/java/final_easy/Main.java
@@ -0,0 +1,9 @@
+package final_easy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
