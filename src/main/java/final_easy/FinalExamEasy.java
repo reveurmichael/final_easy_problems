73dd9a698fa1104b2fe9b09efdcbb3f7d1622f1b
Lunde CHEN <lunde.chen@yahoo.fr>
2020-12-10 18:27:16 +0800

    m

--- src/main/java/final_easy/FinalExamEasy.java
+++ src/main/java/final_easy/FinalExamEasy.java
@@ -14,7 +14,7 @@ class MyData {
     }
 
     public String toString(){
-        return "(" + this.value + ")";
+        return "(MyData, value = " + this.value + ")";
     }
 }
 
@@ -88,12 +88,9 @@ public class FinalExamEasy {
     }
 
     public int[][] Question4(int size){
-        int[][] arr2d = new int[size][size];
+        int[][] arr2d = new int[size][size]; // Don't change this line of code.
         /*
-        Write a programs called NumberPatternX
-         that prompts user for the size
-         (a non-negative integer in int); and prints the pattern as shown:
-         size = 8, then the arr2d will be printed as:
+         Represent one triangle in the arr2d, which, when printed, will show this pattern (e.g. size = 8):
          8 7 6 5 4 3 2 1
          7 6 5 4 3 2 1
          6 5 4 3 2 1
@@ -102,16 +99,20 @@ public class FinalExamEasy {
          3 2 1
          2 1
          1
+
+         Note that 0s are printed as " "(space).
+         You don't need to write the print function. Do things with the arr2d then it's OK.
          */
         // TODO: write your code here.
 
-        print2DIntArray(arr2d);
-        return arr2d;
+        print2DIntArray(arr2d); // Don't change this line of code.
+        return arr2d; // Don't change this line of code.
     }
 
     public int[][] Question5(int size){
-        int[][] arr2d = new int[size][size * 2];
+        int[][] arr2d = new int[size][size * 2]; // Don't change this line of code.
         /*
+         Represent two triangles in the arr2d, which, when printed, will show this pattern (e.g. size = 8):
          8 7 6 5 4 3 2 1 8 7 6 5 4 3 2 1
          7 6 5 4 3 2 1   7 6 5 4 3 2 1
          6 5 4 3 2 1     6 5 4 3 2 1
@@ -120,42 +121,73 @@ public class FinalExamEasy {
          3 2 1           3 2 1
          2 1             2 1
          1               1
+
+         Note that 0s are printed as " "(space).
+         You don't need to write the print function. Do things with the arr2d then it's OK.
          */
         // TODO: write your code here.
 
-        print2DIntArray(arr2d);
-        return arr2d;
+        print2DIntArray(arr2d); // Don't change this line of code.
+        return arr2d; // Don't change this line of code.
     }
 
-    public HashMap<String, MyData> Question6(){
-        HashMap<String, MyData> hashMap = new HashMap<String, MyData>();
+    public HashMap<String, MyData> Question6(int N){
+        HashMap<String, MyData> hashMap = new HashMap<String, MyData>(); // Don't change this line of code.
         /*
-        Hashmap<String, MyData>
-        One: 1
-        Two: 9
-        Three: 27
-        Four: 81
-        Five: 243
+        TODO:
+         Add elements to the hashMap (of type Hashmap<String, MyData>), so that the hashmap will have contents that follow this pattern:
+
+        if N = 2:
+        "One": (MyData, value = 2)
+        "Two": (MyData, value = 4)
+        "Three": (MyData, value = 8)
+        "Four": (MyData, value = 16)
+
+        if N = 3:
+        "One": (MyData, value = 3)
+        "Two": (MyData, value = 9)
+        "Three": (MyData, value = 27)
+        "Four": (MyData, value = 81)
+
+        if N = 4:
+        "One": (MyData, value = 4)
+        "Two": (MyData, value = 16)
+        "Three": (MyData, value = 64)
+        "Four": (MyData, value = 256)
+
+        if N = 10:
+        "One": (MyData, value = 10)
+        "Two": (MyData, value = 100)
+        "Three": (MyData, value = 1000)
+        "Four": (MyData, value = 10000)
+
+        Note that there are only four entries ("One", "Two", "Three", "Four") in the hashMap.
          */
+
         // TODO: write your code here.
 
-        return hashMap;
+        return hashMap; // Don't change this line of code.
     }
 
     public MyData[] sortArrayUsingBubbleOrSelectionSort(MyData[] arrMyData){
         /*
+        TODO:
          Sort an array of MyData, using bubblesort or selectionsort.
          ATTENTION: Please don't modify anything on the MyData class.
          On the contrary, you can add functions to the FinalExamEasy class.
+
+         Note that arrMyData is an parameter, as well as the returned value.
         */
         // TODO: write your code here.
 
-        return arrMyData;
+        return arrMyData; // Don't change this line of code.
     }
 
     public MyData Question8(MyData[] arrMyData){
         /*
-         Find the maximum number of an array of MyData
+        TODO:
+         Find the maximum number of an array of MyData (arrMyData).
+         Note that returned type is MyData.
         */
         // TODO: write your code here.
 
@@ -164,7 +196,9 @@ public class FinalExamEasy {
 
     public MyData Question9(MyData[] arrMyData){
         /*
+        TODO:
          Find the 2nd maximum number of an array of MyData (arrMyData)
+         Note that returned type is MyData.
         */
         // TODO: write your code here.
 
